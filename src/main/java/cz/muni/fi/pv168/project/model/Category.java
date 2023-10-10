55eb1be1de0b1f053b08156df497cceff6f63fa4
Matúš Jakuboc <xjakuboc@fi.muni.cz>
2023-10-10 13:51:38 +0200

    ADD color to category

--- src/main/java/cz/muni/fi/pv168/project/model/Category.java
+++ src/main/java/cz/muni/fi/pv168/project/model/Category.java
@@ -1,17 +1,33 @@
 package cz.muni.fi.pv168.project.model;
 
+import java.awt.*;
+
 public class Category {
     private String name;
+    private Color color;
+    private static final Color DEFAULT_COLOR = Color.WHITE;
+    public Category(String name) {
+        this.name = name;
+        this.color = DEFAULT_COLOR;
+    }
+    public Category(String name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
 
     public String getName() {
         return name;
     }
 
-    public Category(String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
