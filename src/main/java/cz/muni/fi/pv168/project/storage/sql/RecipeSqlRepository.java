b1dd65435e8bc579bf02c0d0f0af219843437b9f
Peter Drazkovec <xdrazkov@fi.muni.cz>
2023-11-27 15:11:44 +0100

    Create RecipeDAO & related classes

--- src/main/java/cz/muni/fi/pv168/project/storage/sql/RecipeSqlRepository.java
+++ src/main/java/cz/muni/fi/pv168/project/storage/sql/RecipeSqlRepository.java
@@ -4,9 +4,13 @@ import cz.muni.fi.pv168.project.model.Recipe;
 import cz.muni.fi.pv168.project.repository.Repository;
 import cz.muni.fi.pv168.project.storage.sql.dao.DataAccessObject;
 import cz.muni.fi.pv168.project.storage.sql.dao.DataStorageException;
+import cz.muni.fi.pv168.project.storage.sql.dao.RecipeIngredientDao;
 import cz.muni.fi.pv168.project.storage.sql.entity.RecipeEntity;
+import cz.muni.fi.pv168.project.storage.sql.entity.RecipeIngredientEntity;
 import cz.muni.fi.pv168.project.storage.sql.entity.mapper.EntityMapper;
+import cz.muni.fi.pv168.project.storage.sql.entity.mapper.RecipeIngredientMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,13 +22,19 @@ import java.util.Optional;
 public class RecipeSqlRepository implements Repository<Recipe> {
 
     private final DataAccessObject<RecipeEntity> recipeDao;
+    private final RecipeIngredientDao recipeIngredientDao;
     private final EntityMapper<RecipeEntity, Recipe> recipeMapper;
+    private final RecipeIngredientMapper recipeIngredientMapper;
 
     public RecipeSqlRepository(
             DataAccessObject<RecipeEntity> recipeDao,
-            EntityMapper<RecipeEntity, Recipe> recipeMapper) {
+            RecipeIngredientDao recipeIngredientDao,
+            EntityMapper<RecipeEntity, Recipe> recipeMapper,
+            RecipeIngredientMapper recipeIngredientMapper) {
         this.recipeDao = recipeDao;
         this.recipeMapper = recipeMapper;
+        this.recipeIngredientDao = recipeIngredientDao;
+        this.recipeIngredientMapper = recipeIngredientMapper;
     }
 
     @Override
@@ -33,12 +43,17 @@ public class RecipeSqlRepository implements Repository<Recipe> {
                 .findAll()
                 .stream()
                 .map(recipeMapper::mapToBusiness)
+                .peek(recipe -> recipe.setIngredients(recipeIngredientMapper.mapToBusiness(recipeIngredientDao.findByRecipeGuid(recipe.getGuid()))))
                 .toList();
     }
 
     @Override
     public void create(Recipe newEntity) {
         recipeDao.create(recipeMapper.mapNewEntityToDatabase(newEntity));
+        ArrayList<RecipeIngredientEntity> recipeIngredients = RecipeIngredientMapper.getRecipeIngredientEntities(newEntity);
+        for (RecipeIngredientEntity recipeIngredientEntity : recipeIngredients) {
+            recipeIngredientDao.create(recipeIngredientEntity);
+        }
     }
 
     @Override
@@ -48,16 +63,29 @@ public class RecipeSqlRepository implements Repository<Recipe> {
         var updatedRecipe = recipeMapper.mapExistingEntityToDatabase(entity, existingRecipe.guid());
 
         recipeDao.update(updatedRecipe);
+
+        ArrayList<RecipeIngredientEntity> recipeIngredients = RecipeIngredientMapper.getRecipeIngredientEntities(entity);
+        for (RecipeIngredientEntity recipeIngredientEntity : recipeIngredients) {
+            var existsRecipeIngredient = recipeIngredientDao.findByGuid(entity.getGuid());
+
+            if (existsRecipeIngredient.isPresent()) {
+                recipeIngredientDao.update(recipeIngredientEntity);
+            } else {
+                recipeIngredientDao.create(recipeIngredientEntity);
+            }
+        }
     }
 
     @Override
     public void deleteByGuid(String guid) {
         recipeDao.deleteByGuid(guid);
+        recipeIngredientDao.deleteByRecipeGuid(guid);
     }
 
     @Override
     public void deleteAll() {
         recipeDao.deleteAll();
+        recipeIngredientDao.deleteAll();
     }
 
     @Override
@@ -67,8 +95,15 @@ public class RecipeSqlRepository implements Repository<Recipe> {
 
     @Override
     public Optional<Recipe> findByGuid(String guid) {
-        return recipeDao
+        Optional<Recipe> recipe = recipeDao
                 .findByGuid(guid)
                 .map(recipeMapper::mapToBusiness);
+
+        if (recipe.isPresent()) {
+            Recipe recipeValue = recipe.get();
+            recipeValue.setIngredients(recipeIngredientMapper.mapToBusiness(recipeIngredientDao.findByRecipeGuid(guid)));
+        }
+
+        return recipe;
     }
 }
